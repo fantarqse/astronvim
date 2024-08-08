58fe69518e6b136e31989c897cb54d59816c08f1
fantarqse <fantarose51@gmail.com>
2024-08-08 13:33:01 +0300

    feat: added auto formatting

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -20,8 +20,9 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+          "go",
           "python",
+          "lua",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
